5310ac2b10a039af100314366eb810cf9563918a
mjesan <mjesan152024@bscse.uiu.ac.bd>
2024-04-21 13:14:28 +0600

    Added User Edit Profile

--- app/src/main/java/com/example/agrochain/EditProfileActivity.java
+++ app/src/main/java/com/example/agrochain/EditProfileActivity.java
@@ -1,21 +1,32 @@
 package com.example.agrochain;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
+//FAR-OLpkvxe
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -24,7 +35,15 @@ public class EditProfileActivity extends AppCompatActivity {
     private EditText editTextDivision, editTextDistrict, editTextUpazila, editTextNID;
     private Spinner spinnerGender;
     private Button buttonUpdateProfile, buttonDeleteProfile;
+    private Button btnLogout;
     private String userID;
+    private ImageView imgEditProfilePicture;
+    private Uri selectedImageUri = null;
+    private final ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            uri -> {
+                imgEditProfilePicture.setImageURI(uri);
+                selectedImageUri = uri;
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +64,11 @@ public class EditProfileActivity extends AppCompatActivity {
         spinnerGender = findViewById(R.id.spinnerGender);
         buttonUpdateProfile = findViewById(R.id.buttonUpdateProfile);
         buttonDeleteProfile = findViewById(R.id.buttonDeleteProfile);
+        imgEditProfilePicture = findViewById(R.id.imgEditProfilePicture);
+
+        imgEditProfilePicture.setOnClickListener(v -> {
+            mGetContent.launch("image/*");
+        });
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.gender_array, android.R.layout.simple_spinner_item);
@@ -56,21 +80,42 @@ public class EditProfileActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Error: User ID is missing.", Toast.LENGTH_SHORT).show();
         }
-        buttonUpdateProfile.setOnClickListener(v -> updateUserProfile());
+        buttonUpdateProfile.setOnClickListener(v -> {
+            uploadNewProfilePicture();
+        });
         buttonDeleteProfile.setOnClickListener(v -> deleteUserProfile());
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("AppPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.clear();
+                editor.apply();
+
+                Intent intent = new Intent(EditProfileActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
-    private void updateUserProfile() {
+
+    private void updateUserProfile(String imageUrl) {
         Map<String, Object> userUpdates = new HashMap<>();
         userUpdates.put("name", editTextName.getText().toString());
         userUpdates.put("age", editTextAge.getText().toString());
         userUpdates.put("phone", editTextPhone.getText().toString());
-        // Add other fields
         userUpdates.put("gender", spinnerGender.getSelectedItem().toString());
         userUpdates.put("division", editTextDivision.getText().toString());
         userUpdates.put("district", editTextDistrict.getText().toString());
         userUpdates.put("upazila", editTextUpazila.getText().toString());
         userUpdates.put("NID", editTextNID.getText().toString());
+        if (imageUrl != null) {
+            userUpdates.put("profilePictureUrl", imageUrl);
+        }
 
         db.collection("FAR").document(userID).update(userUpdates).addOnSuccessListener(aVoid -> {
             Toast.makeText(EditProfileActivity.this, "Profile Updated Successfully", Toast.LENGTH_SHORT).show();
@@ -101,6 +146,10 @@ public class EditProfileActivity extends AppCompatActivity {
 
                     setSpinnerSelection(spinnerGender, document.getString("gender"));
 
+                    String imageUrl = document.getString("profilePictureUrl");
+                    if (imageUrl != null && !imageUrl.isEmpty()) {
+                        Glide.with(this).load(imageUrl).apply(new RequestOptions().override(1024, 768)).into(imgEditProfilePicture);
+                    }
                 } else {
                     Toast.makeText(EditProfileActivity.this, "No such user", Toast.LENGTH_SHORT).show();
                 }
@@ -116,4 +165,28 @@ public class EditProfileActivity extends AppCompatActivity {
             spinner.setSelection(position);
         }
     }
+
+    private void uploadNewProfilePicture() {
+        if (selectedImageUri != null) {
+            String fileName = "profile_" + userID + ".jpg";
+            StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("profile_images/" + fileName);
+
+            Log.d("EditProfileActivity", "Uploading new profile picture: " + selectedImageUri.toString());
+
+            fileRef.putFile(selectedImageUri).addOnSuccessListener(taskSnapshot -> {
+                fileRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    String imageUrl = uri.toString();
+                    Log.d("EditProfileActivity", "New profile picture uploaded. Image URL: " + imageUrl);
+                    updateUserProfile(imageUrl);
+                });
+            }).addOnFailureListener(e -> {
+                Log.e("EditProfileActivity", "Upload failed: " + e.getMessage(), e);
+                Toast.makeText(EditProfileActivity.this, "Upload failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            });
+        } else {
+            Log.d("EditProfileActivity", "No new profile picture selected. Updating profile without changing the image.");
+            updateUserProfile(null);
+        }
+    }
+
 }
